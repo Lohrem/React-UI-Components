0c0b7944a2d87896cadaf2698def2d77d17c9171
Lohrem <30306978+Lohrem@users.noreply.github.com>
2019-05-11 13:22:48 -0400

    finished up the styling and buttons for the calculator

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -4,7 +4,11 @@ import './Button.css';
 const ActionBtns = () => {
   return (
     <div className="action-btns">
-      
+      <button className="actionBtn btn" value="/" name="divide">/</button>
+      <button className="actionBtn btn" value="*" name="times">*</button>
+      <button className="actionBtn btn" value="-" name="minus">-</button>
+      <button className="actionBtn btn" value="+" name="plus">+</button>
+      <button className="actionBtn btn" value="=" name="equal">=</button>
     </div>
   )
 }
